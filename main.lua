06dc9541d7536e974298d5343f3c17e4f3395206
snarkusius <marcus@lengquist.se>
2021-02-01 14:54:46 +0100

    firstCommit

---
+++ main.lua
@@ -0,0 +1,22 @@
+print("Hello vorld" .. "god")
+name = "marcus"
+eman = string.reverse(name)
+print("hello".." "..name)
+print(eman)
+-- comment
+add = function(a,b)
+    return a+b
+end
+print(add(1,2))
+print(5~=5 and 7==7)
+animal = "dog"
+if (animal == "dog") 
+    then print("i am runnig") 
+    else
+        print("i am not runnig")
+end
+x=0
+while x < 5
+do  x = x+1
+    print(x)
+end
